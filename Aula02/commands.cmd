1f7a89d6d28639cab4532c1a208090d04bbd6fd5
annaguerrra <annaguerracastro@gmail.com>
2026-05-27 11:15:05 -0300

    testing get and post request

--- Aula02/commands.cmd
+++ Aula02/commands.cmd
@@ -1,2 +1,14 @@
+@REM dar build na img
 docker build -t imagem01:latest ./
+
+@REM  rodar imagem
 docker run imagem01
+
+@REM comando para não ter que ficar mexendo em props
+CMD ["dotnet", "run", "--urls=http://0.0.0.0:[porta em expose]"]
+
+docker compose up --build
+
+docker compose up -d --build
+
+docker compose ps
